f81ef2c87c3190527726f6061caf158c779e14a2
Jason Truong <W0869805@myscc.ca>
2025-09-19 15:26:13 -0400

    Create a new data type with interface User - Has 5 properties (1 optional property) - One is optional boolean property isAdmin

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {User} from './Shared/Models/user';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -16,4 +18,23 @@ export class App {
   // name : string = "bob"
 
   new : string = "Truong"
+
+  // declare instances of user object
+  user1 : User = {id: 1, firstName: "Jason", lastName: "Truong", department: "Computer Programming", isAdmin: false};
+  user2 : User = {id: 2, firstName: "Joe", lastName: "Schmoe", department: "Mobile Apps Development", isAdmin: true};
+  user3 : User = {id: 3, firstName: "Dillon", lastName: "Bell", department: "Computer Programming", isAdmin: false};
+  user4 : User = {id: 4, firstName: "Adam", lastName: "Larsh", department: "Computer Programming", isAdmin: false};
+  user5 : User = {id: 5, firstName: "Owen", lastName: "Trulieb", department: "Computer Programming", isAdmin: false};
+  user6 : User = {id: 6, firstName: "Walter", lastName: "White", department: "Mobile Apps Development", isAdmin: true};
+
+  // initialize array with instances of user object
+  userList: User[] =
+    [
+      this.user1,
+      this.user2,
+      this.user3,
+      this.user4,
+      this.user5,
+      this.user6
+    ]
 }
