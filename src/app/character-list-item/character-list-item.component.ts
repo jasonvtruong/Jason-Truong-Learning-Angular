15de98eabc4a24881a0b5e47160d4432820fedae
Jason Truong <W0869805@myscc.ca>
2025-09-28 15:22:57 -0400

    Created 2 new components - character-list, character-list-item

---
+++ src/app/character-list-item/character-list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-list-item',
+  imports: [],
+  templateUrl: './character-list-item.component.html',
+  styleUrl: './character-list-item.component.css'
+})
+export class CharacterListItemComponent {
+
+}
