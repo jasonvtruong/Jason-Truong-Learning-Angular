958ea2bdd00d5fa3abfcf9791e3283feb41acd4e
Jason Truong <W0869805@myscc.ca>
2025-09-30 10:36:53 -0400

    Fixed alternating colors - Merged the 2 item arrays - Moved HTML tags (interpolation) and CSS styling from content-list into the list item component's HTML and CSS - Apply alternating colors to all items, instead of only the bottom items

--- src/app/character-list-item/character-list-item.component.ts
+++ src/app/character-list-item/character-list-item.component.ts
@@ -1,9 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {Character} from '../Shared/Models/character';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-character-list-item',
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './character-list-item.component.html',
   styleUrl: './character-list-item.component.css'
 })
--- src/app/character-list/character-list.component.ts
+++ src/app/character-list/character-list.component.ts
@@ -14,29 +14,15 @@ import {CharacterListItemComponent} from '../character-list-item/character-list-
   styleUrl: './character-list.component.css'
 })
 export class CharacterListComponent {
-  // declare instances of character object
-  character1 : Character = {name: "Tsukasa Kadoya", gender: "Male", riderName: "Kamen Rider Decade", type: "Primary", motif: "Cards, Barcode", isARider: true, imageName: "TsukasaKadoya"};
-  character2 : Character = {name: "Shotaro Hidari", gender: "Male", riderName: "Kamen Rider Double", type: "Primary", motif: "Grasshopper, Wind, Joker", isARider: true, imageName: "ShotaroHidari"};
-  character3 : Character = {name: "Philip", gender: "Male", riderName: "Kamen Rider Double", type: "Primary", motif: "Grasshopper, Wind, Joker", isARider: true, imageName: "Philip"};
-  character4 : Character = {name: "Eiji Hino", age: 21, gender: "Male", riderName: "Kamen Rider OOO", type: "Primary", motif: "Hawk, Tiger, Grasshopper", isARider: true, imageName: "EijiHino"};
-  character5 : Character = {name: "Yusuke Godai", age: 25, gender: "Male", riderName: "Kamen Rider Kuuga", type: "Primary", motif: "Stag beetle", isARider: true, imageName: "YusukeGodai"};
-  character6 : Character = {name: "Takumi Inui", age: 18, gender: "Male", riderName: "Kamen Rider Faiz", type: "Primary", motif: "Phi symbol, Shark", isARider: true, imageName: "TakumiInui"};
-
   // initialize array with instances of character object
   characterList: Character[] =
     [
-      this.character1,
-      this.character2,
-      this.character3,
-      this.character4,
-      this.character5,
-      this.character6,
-    ]
-
-
-  // initialize a new array with 4 items
-  characterList2: Character[] =
-    [
+      {name: "Tsukasa Kadoya", gender: "Male", riderName: "Kamen Rider Decade", type: "Primary", motif: "Cards, Barcode", isARider: true, imageName: "TsukasaKadoya"},
+      {name: "Shotaro Hidari", gender: "Male", riderName: "Kamen Rider Double", type: "Primary", motif: "Grasshopper, Wind, Joker", isARider: true, imageName: "ShotaroHidari"},
+      {name: "Philip", gender: "Male", riderName: "Kamen Rider Double", type: "Primary", motif: "Grasshopper, Wind, Joker", isARider: true, imageName: "Philip"},
+      {name: "Eiji Hino", age: 21, gender: "Male", riderName: "Kamen Rider OOO", type: "Primary", motif: "Hawk, Tiger, Grasshopper", isARider: true, imageName: "EijiHino"},
+      {name: "Yusuke Godai", age: 25, gender: "Male", riderName: "Kamen Rider Kuuga", type: "Primary", motif: "Stag beetle", isARider: true, imageName: "YusukeGodai"},
+      {name: "Takumi Inui", age: 18, gender: "Male", riderName: "Kamen Rider Faiz", type: "Primary", motif: "Phi symbol, Shark", isARider: true, imageName: "TakumiInui"},
       {name: "Daiki Kaito", gender: "Male", riderName: "Kamen Rider Diend", type: "Secondary", motif: "Cards, Barcode, Blue Shieldbug", isARider: true, imageName: "DaikiKaito"},
       {name: "Ren Akiyama", age: 24, gender: "Male", riderName: "Kamen Rider Knight", type: "Secondary", motif: "Knight, Bat", isARider: true, imageName: "RenAkiyama"},
       {name: "Ryu Terui", gender: "Male", riderName: "Kamen Rider Accel", type: "Secondary", motif: "Motorcycle", isARider: true, imageName: "RyuTendo"},
