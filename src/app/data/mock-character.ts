69a5421b6ff03ab2cf91c7973b56a4d19bdb79d6
Jason Truong <W0869805@myscc.ca>
2025-10-05 18:03:19 -0400

    Moved character array contents into its own mock TypeScript file Create a service named Character

---
+++ src/app/data/mock-character.ts
@@ -0,0 +1,17 @@
+// import interface
+import {Character} from '../Shared/Models/character';
+
+// initialize array with instances of character object
+export const characterList: Character[] =
+  [
+    {name: "Tsukasa Kadoya", gender: "Male", riderName: "Kamen Rider Decade", type: "Primary", motif: "Cards, Barcode", isARider: true, imageName: "TsukasaKadoya"},
+    {name: "Shotaro Hidari", gender: "Male", riderName: "Kamen Rider Double", type: "Primary", motif: "Grasshopper, Wind, Joker", isARider: true, imageName: "ShotaroHidari"},
+    {name: "Philip", gender: "Male", riderName: "Kamen Rider Double", type: "Primary", motif: "Grasshopper, Wind, Joker", isARider: true, imageName: "Philip"},
+    {name: "Eiji Hino", age: 21, gender: "Male", riderName: "Kamen Rider OOO", type: "Primary", motif: "Hawk, Tiger, Grasshopper", isARider: true, imageName: "EijiHino"},
+    {name: "Yusuke Godai", age: 25, gender: "Male", riderName: "Kamen Rider Kuuga", type: "Primary", motif: "Stag beetle", isARider: true, imageName: "YusukeGodai"},
+    {name: "Takumi Inui", age: 18, gender: "Male", riderName: "Kamen Rider Faiz", type: "Primary", motif: "Phi symbol, Shark", isARider: true, imageName: "TakumiInui"},
+    {name: "Daiki Kaito", gender: "Male", riderName: "Kamen Rider Diend", type: "Secondary", motif: "Cards, Barcode, Blue Shieldbug", isARider: true, imageName: "DaikiKaito"},
+    {name: "Ren Akiyama", age: 24, gender: "Male", riderName: "Kamen Rider Knight", type: "Secondary", motif: "Knight, Bat", isARider: true, imageName: "RenAkiyama"},
+    {name: "Ryu Terui", gender: "Male", riderName: "Kamen Rider Accel", type: "Secondary", motif: "Motorcycle", isARider: true, imageName: "RyuTendo"},
+    {name: "Masato Kusaka", gender: "Male", riderName: "Kamen Rider Kaixa", type: "Secondary", motif: "Chi symbol, Hornet", isARider: true, imageName: "MasatoKusaka"}
+  ]
