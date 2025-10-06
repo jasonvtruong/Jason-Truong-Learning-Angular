4e8ff965edc24ab6084b0ae8e824a4c6897c37f1
Jason Truong <W0869805@myscc.ca>
2025-10-06 14:53:12 -0400

    Add a content-list item at the top that is populated after retrieving a Character object - Add service to app component using dependency injection - Create a new Character object - Modify the new Character object to store data of a character using specific id

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import {Character} from './Shared/Models/character';
 import {NgForOf, NgIf} from '@angular/common';
 import {CharacterListComponent} from './character-list/character-list.component';
+import {CharacterListItemComponent} from './character-list-item/character-list-item.component';
+import {characterList} from './data/mock-character';
+import {CharacterService} from './Services/character.service';
 
 @Component({
   selector: 'app-root',
-  imports: [NgForOf, NgIf, CharacterListComponent],
+  imports: [NgForOf, NgIf, CharacterListComponent, CharacterListItemComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -14,5 +17,21 @@ export class App {
   protected readonly title = signal('Jason-Truong-Learning-Angular');
 
 
+  // inject dependency by declaring it in constructor
+  constructor(private characterService: CharacterService) {}
+
+
+  // create character object to populate the content-list item
+  public newCharacter: Character | undefined;
+
+  //Set up the ngonit to listen for a getCharacterbyId
+  ngOnInit(){
+    this.characterService.getCharacterById(6).subscribe(character =>{
+      //Save it in var
+      this.newCharacter = character;
+    })
+  }
+
+  //Call that var in the propety binding in the html
 
 }
