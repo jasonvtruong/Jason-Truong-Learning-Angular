71b77671ecf771d17d9bbc754f386a541bef7103
Jason Truong <W0869805@myscc.ca>
2025-11-01 14:49:20 -0400

    Create an InMemoryDataService - define mock data for API

---
+++ src/app/Services/in-memory-data.service.ts
@@ -0,0 +1,21 @@
+import {InMemoryDbService} from 'angular-in-memory-web-api';
+import {Character} from '../Shared/Models/character';
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb(): {characterList: Character[]} {
+    const characterList: Character[] =
+      [
+        {id: 10, name: "Tsukasa Kadoya", gender: "Male", riderName: "Kamen Rider Decade", type: "Primary", motif: "Cards, Barcode", isARider: true, imageName: "TsukasaKadoya"},
+        {id: 11, name: "Shotaro Hidari", gender: "Male", riderName: "Kamen Rider Double", type: "Primary", motif: "Grasshopper, Wind, Joker", isARider: true, imageName: "ShotaroHidari"},
+        {id: 12, name: "Philip", gender: "Male", riderName: "Kamen Rider Double", type: "Primary", motif: "Grasshopper, Wind, Joker", isARider: true, imageName: "Philip"},
+        {id: 14, name: "Eiji Hino", age: 21, gender: "Male", riderName: "Kamen Rider OOO", type: "Primary", motif: "Hawk, Tiger, Grasshopper", isARider: true, imageName: "EijiHino"},
+        {id: 1, name: "Yusuke Godai", age: 25, gender: "Male", riderName: "Kamen Rider Kuuga", type: "Primary", motif: "Stag beetle", isARider: true, imageName: "YusukeGodai"},
+        {id: 5, name: "Takumi Inui", age: 18, gender: "Male", riderName: "Kamen Rider Faiz", type: "Primary", motif: "Phi symbol, Shark", isARider: true, imageName: "TakumiInui"},
+        {id: 11, name: "Daiki Kaito", gender: "Male", riderName: "Kamen Rider Diend", type: "Secondary", motif: "Cards, Barcode, Blue Shieldbug", isARider: true, imageName: "DaikiKaito"},
+        {id: 4, name: "Ren Akiyama", age: 24, gender: "Male", riderName: "Kamen Rider Knight", type: "Secondary", motif: "Knight, Bat", isARider: true, imageName: "RenAkiyama"},
+        {id: 13, name: "Ryu Terui", gender: "Male", riderName: "Kamen Rider Accel", type: "Secondary", motif: "Motorcycle", isARider: true, imageName: "RyuTendo"},
+        {id: 6, name: "Masato Kusaka", gender: "Male", riderName: "Kamen Rider Kaixa", type: "Secondary", motif: "Chi symbol, Hornet", isARider: true, imageName: "MasatoKusaka"}
+      ];
+    return {characterList};
+  }
+}
